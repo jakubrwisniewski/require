e52afd7aeb1bf4747e72dcb79b31ef237b414e4c
Jakub Wiśniewski <jakub.r.wisniewski@gmail.com>
2019-09-04 18:04:51 +0200

    require.js created

---
+++ require.js
@@ -0,0 +1,21 @@
+const def = {};
+const cached = {};
+
+function require(name) {
+	if(!def[name]) {
+		throw new Error(`Module ${name} is not implemented`);
+	}
+
+	if(!cached[name]) {
+		cached[name] = def[name]();
+	}
+
+	return cached[name];
+}
+
+function define(name, value) {
+	def[name] = value;
+}
+
+window.require = require;
+window.define = define;
