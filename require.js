19ab289a612ab34e2d149c4eeb0c7c0cba2c58e8
Jakub Wiśniewski <jakub.r.wisniewski@gmail.com>
2019-09-25 14:56:50 +0200

    hide code in personal scope

--- require.js
+++ require.js
@@ -1,21 +1,23 @@
-const def = {};
-const cached = {};
+(function() {
+	const def = {};
+	const cached = {};
 
-function require(name) {
-	if(!def[name]) {
-		throw new Error(`Module ${name} is not implemented`);
+	function require(name) {
+		if(!def[name]) {
+			throw new Error(`Module ${name} is not implemented`);
+		}
+
+		if(!cached[name]) {
+			cached[name] = def[name]();
+		}
+
+		return cached[name];
 	}
 
-	if(!cached[name]) {
-		cached[name] = def[name]();
+	function define(name, value) {
+		def[name] = value;
 	}
 
-	return cached[name];
-}
-
-function define(name, value) {
-	def[name] = value;
-}
-
-window.require = require;
-window.define = define;
+	window.require = require;
+	window.define = define;
+})();
